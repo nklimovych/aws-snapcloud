946f970a234231116b3cfb4541a9c454883ea86e
Nazar Klimovych <rksvjdbx@gmail.com>
2024-06-28 20:46:15 +0300

    Add image entity, repository and liquibase changelog

---
+++ src/main/java/com/aws/snapcloud/entity/Image.java
@@ -0,0 +1,39 @@
+package com.aws.snapcloud.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @CreationTimestamp
+    private LocalDateTime uploadedAt;
+
+    @Column(nullable = false)
+    private String url;
+}
