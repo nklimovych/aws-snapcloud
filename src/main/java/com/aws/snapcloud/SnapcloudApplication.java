4b37dae4aff24f157b6e939ca9db265cc3c0250f
Nazar Klimovych <rksvjdbx@gmail.com>
2024-06-28 15:19:22 +0300

    Add Spring Boot initialization setup

---
+++ src/main/java/com/aws/snapcloud/SnapcloudApplication.java
@@ -0,0 +1,13 @@
+package com.aws.snapcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/snapcloud/SnapcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.snapcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
