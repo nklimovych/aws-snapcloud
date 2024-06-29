d68cb8733b60e8a4bd6c363b3c91982239632adf
Nazar Klimovych <92024646+nklimovych@users.noreply.github.com>
2024-06-30 00:29:45 +0300

    Add Image and Label models and related repository (#2)

---
+++ src/main/java/com/aws/snapcloud/entity/Label.java
@@ -0,0 +1,37 @@
+package com.aws.snapcloud.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "labels")
+public class Label {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "labels")
+    private Set<Image> images = new HashSet<>();
+}
--- src/test/java/com/aws/snapcloud/SnapcloudApplicationTests.java
+++ src/test/java/com/aws/snapcloud/SnapcloudApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SnapcloudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
